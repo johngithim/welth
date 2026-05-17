549845948ff9bf66aef00bd990bf973676825eb5
john <johnmuller0910@gmail.com>
2026-05-18 01:37:12 +0300

    vercel error

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,12 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
+import { db } from "@/lib/prisma";
+import * as url from "node:url";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
     seed: "tsx prisma/seed.ts",
-  },
-  datasource: {
-    url: env("DATABASE_URL"),
   },
 });
