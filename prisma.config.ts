6e3b1877cf129b58f8b68d39a2fe8c5d8aa40e6e
john <johnmuller0910@gmail.com>
2026-05-18 01:49:33 +0300

    God help us!!

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,13 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
-import { db } from "@/lib/prisma";
-import * as url from "node:url";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
     seed: "tsx prisma/seed.ts",
+  },
+  datasource: {
+    url: env("DATABASE_URL"),
   },
 });
